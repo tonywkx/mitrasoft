3cc458e8a4ca7abce14f7306a7f8e2cdbf17921e
tonywkx <tonywk@ya.ru>
2023-06-05 01:01:40 +0400

    fix: for deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
      <App/>
-    </BrowserRouter>
+    </HashRouter>
     </Provider>
     
   </React.StrictMode>
